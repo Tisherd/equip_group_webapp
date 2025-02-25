94af95ef81a5dbf800c6535f49aa358e63aef1cd
Tisherd <mtisherd@gmail.com>
2025-02-25 12:38:53 +0800

    fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
     plugins: [
         laravel({
             input: 'resources/js/app.js',
@@ -26,4 +26,4 @@ export default defineConfig({
             },
         },
     }),
-});
+}));
