34e662e39576ead7c231e89607800d02277f035d
Tisherd <mtisherd@gmail.com>
2025-02-25 15:10:59 +0800

    update relations in Models

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Group extends Model
 {
     /** @use HasFactory<\Database\Factories\GroupFactory> */
     use HasFactory;
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class, 'id_group');
+    }
 }
--- app/Models/Price.php
+++ app/Models/Price.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Price extends Model
 {
     /** @use HasFactory<\Database\Factories\PriceFactory> */
     use HasFactory;
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'id_product');
+    }
 }
