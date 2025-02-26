71596415a4e29ea807ac80a6db66b6561be9808a
Tisherd <mtisherd@gmail.com>
2025-02-26 12:09:06 +0800

    create Service/GroupHierarchy

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -5,14 +5,29 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Services\GroupHierarchy;
 
 class Group extends Model
 {
     /** @use HasFactory<\Database\Factories\GroupFactory> */
     use HasFactory;
 
+    protected $fillable = ['id_parent', 'name'];
+
     public function products(): HasMany
     {
         return $this->hasMany(Product::class, 'id_group');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Group::class, 'id_parent');
+    }
+
+    public function scopeWithHierarchy($query): array
+    {
+        $groups = $query->withCount('products')->get();
+        $hierarchy = new GroupHierarchy($groups);
+        return $hierarchy->getHierarchy();
     }
 }
---
+++ app/Services/GroupHierarchy.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Database\Eloquent\Collection;
+
+class GroupHierarchy
+{
+    protected Collection $groups;
+    protected array $groupsById = [];
+    protected array $hierarchy = [];
+
+    public function __construct(Collection $groups)
+    {
+        $this->groups = $groups;
+        $this->buildHierarchy();
+    }
+
+    protected function buildHierarchy()
+    {
+        // Индексируем группы по id
+        foreach ($this->groups as $group) {
+            $this->groupsById[$group->id] = [
+                'id' => $group->id,
+                'name' => $group->name,
+                'products_quantity' => $group->products_count ?? 0,
+                'id_parent' => $group->id_parent,
+                'children' => [],
+                'full_group_ids' => [$group->id],
+            ];
+        }
+
+        // Строим дерево
+        foreach ($this->groupsById as &$group) {
+            if ($group['id_parent'] && isset($this->groupsById[$group['id_parent']])) {
+                $this->groupsById[$group['id_parent']]['children'][] = &$group;
+            } else {
+                $this->hierarchy[] = &$group;
+            }
+        }
+
+        // Добавляем full_group_ids
+        foreach ($this->hierarchy as &$rootGroup) {
+            $this->populateFullGroupIds($rootGroup);
+        }
+    }
+
+    protected function populateFullGroupIds(&$group)
+    {
+        foreach ($group['children'] as &$child) {
+            $this->populateFullGroupIds($child);
+            $group['full_group_ids'] = array_merge($group['full_group_ids'], $child['full_group_ids']);
+            $group['products_quantity'] += $child['products_quantity']; // Суммируем товары из подгрупп
+        }
+    }
+
+    public function getHierarchy(): array
+    {
+        return $this->hierarchy;
+    }
+
+    public function getFullGroupIds($groupId): array
+    {
+        return $this->groupsById[$groupId]['full_group_ids'] ?? [];
+    }
+}
